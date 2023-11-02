5cffd492b03eef48afc3663e2d4ef5d63c4dd312
MRyan <770255975@qq.com>
2023-11-02 10:46:33 +0800

    style:update

--- hammerspoon-config/.hammerspoon/ime_switcher.lua
+++ hammerspoon-config/.hammerspoon/ime_switcher.lua
@@ -1,7 +1,8 @@
 ----------------------------------------------------------------------------------------------------
 -------------------------------------------- 根据APP自动切换输入法 ------------------------------------
 ----------------------------------------------------------------------------------------------------
-local ime_switcher = {}
+ime_switcher = {}
+ime_switcher.excludingDefaultIme = ''
 
 function ime_switcher.Chinese()
     hs.keycodes.currentSourceID("com.sogou.inputmethod.sogou.pinyin")
@@ -11,34 +12,38 @@ function ime_switcher.English()
     hs.keycodes.currentSourceID("com.apple.keylayout.ABC")
 end
 
-ime_switcher.app2Ime = {{'/Applications/iTerm.app', 'English'}, {'/Applications/Visual Studio Code.app', 'English'},
-                        {'/Applications/Google Chrome.app', 'Chinese'}, {'/Applications/WeChat.app', 'Chinese'},
-                        {'/Applications/System Preferences.app', 'English'}, {'/Applications/TickTick.app', 'Chinese'},
-                        {'/Applications/企业微信.app', 'Chinese'}, {'/Applications/语雀.app', 'Chinese'},
-                        {'/Applications/印象笔记.app', 'Chinese'}, {'/Applications/Dash.app', 'English'},
-                        {'/Applications/Android Studio.app', 'English'}, {'/Applications/Bob.app', 'Chinese'},
-                        {'/Applications/IntelliJ IDEA.app', 'English'}, {'/Applications/QQ.app', 'Chinese'},
-                        {'/Applications/Typora.app', 'Chinese'}, {'/Applications/Alfred 5.app', 'English'},
-                        {'/Applications/starfish.app', 'Chinese'}, {'/Applications/Microsoft Edge.app', 'Chinese'},
-                        {'/Applications/Postman.app', 'English'}, {'/Applications/Safari.app', 'Chinese'},
-                        {'/Applications/Obsidian.app', 'English'}, {'/Applications/Navicat Premium.app', 'English'},
-                        {'/Applications/Lark.app', 'Chinese'}, {'/Applications/wechatwebdevtools.app', 'English'}}
+ime_switcher.app2Ime = ime_switcher.app2Ime or
+                           {{'/Applications/iTerm.app', 'English'}, {'/Applications/System Preferences.app', 'English'}}
 
 function ime_switcher.updateFocusAppInputMethod()
-    local focusAppPath = hs.window.frontmostWindow():application():path()
-    for index, app in pairs(ime_switcher.app2Ime) do
-        local appPath = app[1]
-        local expectedIme = app[2]
+    local excludingDefaultIme = ime_switcher.excludingDefaultIme
+    print("excludingDefaultIme: " .. tostring(excludingDefaultIme))
 
-        if focusAppPath == appPath then
-            if expectedIme == 'English' then
-                ime_switcher.English()
-                print("切换英文 appPath:" .. appPath)
-            else
-                ime_switcher.Chinese()
-                print("切换中文 appPath:" .. appPath)
+    -- 如果excludingDefaultIme没有配置，则遍历app2Ime
+    if excludingDefaultIme == nil or type(excludingDefaultIme) ~= "string" or excludingDefaultIme == "" then
+        for index, app in pairs(ime_switcher.app2Ime) do
+            local appPath = app[1]
+            local expectedIme = app[2]
+
+            if focusAppPath == appPath then
+                if expectedIme == 'English' then
+                    ime_switcher.English()
+                    print("切换英文 appPath:" .. appPath)
+                else
+                    ime_switcher.Chinese()
+                    print("切换中文 appPath:" .. appPath)
+                end
+                return
             end
-            break
+        end
+    else
+        -- 如果excludingDefaultIme被配置了，则直接切换到默认输入法
+        if excludingDefaultIme == 'English' then
+            ime_switcher.English()
+            print("默认切换英文")
+        elseif excludingDefaultIme == 'Chinese' then
+            ime_switcher.Chinese()
+            print("默认切换中文")
         end
     end
 end
