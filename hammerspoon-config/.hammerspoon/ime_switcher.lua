367e9cd9047c19ea27a3ba9d491206c170dec608
MRyan <770255975@qq.com>
2023-11-02 11:46:52 +0800

    fix:bug

--- hammerspoon-config/.hammerspoon/ime_switcher.lua
+++ hammerspoon-config/.hammerspoon/ime_switcher.lua
@@ -17,15 +17,20 @@ ime_switcher.app2Ime = ime_switcher.app2Ime or
 
 function ime_switcher.updateFocusAppInputMethod()
     local excludingDefaultIme = ime_switcher.excludingDefaultIme
-    print("excludingDefaultIme: " .. tostring(excludingDefaultIme))
+    local focusAppPath = hs.window.frontmostWindow():application():path()
 
-    -- 如果excludingDefaultIme没有配置，则遍历app2Ime
-    if excludingDefaultIme == nil or type(excludingDefaultIme) ~= "string" or excludingDefaultIme == "" then
+    -- 检查 ime_switcher.app2Ime 是否有值
+    if ime_switcher.app2Ime == nil or next(ime_switcher.app2Ime) == nil then
+        -- ime_switcher.app2Ime 是空的或者没有定义，走默认切换逻辑
+        ime_switcher.switch(excludingDefaultIme)
+    else
+        -- ime_switcher.app2Ime 有值，检查当前应用程序是否在表中
+        local found = false
         for index, app in pairs(ime_switcher.app2Ime) do
             local appPath = app[1]
             local expectedIme = app[2]
-
             if focusAppPath == appPath then
+                found = true
                 if expectedIme == 'English' then
                     ime_switcher.English()
                     print("切换英文 appPath:" .. appPath)
@@ -33,18 +38,23 @@ function ime_switcher.updateFocusAppInputMethod()
                     ime_switcher.Chinese()
                     print("切换中文 appPath:" .. appPath)
                 end
-                return
+                break
             end
         end
-    else
-        -- 如果excludingDefaultIme被配置了，则直接切换到默认输入法
-        if excludingDefaultIme == 'English' then
-            ime_switcher.English()
-            print("默认切换英文")
-        elseif excludingDefaultIme == 'Chinese' then
-            ime_switcher.Chinese()
-            print("默认切换中文")
+        if not found then
+            -- 当前应用程序不在 app2Ime 表中，走默认切换逻辑
+            ime_switcher.switch(excludingDefaultIme)
         end
+    end
+end
+
+function ime_switcher.switch(excludingDefaultIme)
+    if excludingDefaultIme == 'English' then
+        ime_switcher.English()
+        print("默认切换英文")
+    elseif excludingDefaultIme == 'Chinese' then
+        ime_switcher.Chinese()
+        print("默认切换中文")
     end
 end
 
